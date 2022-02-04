b24273e80b1e4656ab4b27f7688532983c4fd535
GleidsonDuarte <work.gleidsonduarte@hotmail.com>
2022-02-04 14:49:50 -0300

    fix(ProviderService): Adicionado async await no service

--- src/service/services/ProviderService.ts
+++ src/service/services/ProviderService.ts
@@ -2,34 +2,34 @@ import { Provider } from "../../domain/entities/Provider";
 import ProviderRepository from "../../infrastructure/data/repositories/ProviderRepository";
 
 class ProviderService {
-    public getAllProviders() {
-        return ProviderRepository.findAll();
+    public async getAllProviders() {
+        return await ProviderRepository.findAll();
     }
 
-    public getProviderById(id: string) {
-        return ProviderRepository.findById(id);
+    public async getProviderById(id: string) {
+        return await ProviderRepository.findById(id);
     }
 
-    public getProviderByFirstName(firstName: string) {
-        return ProviderRepository.findByFirstName(firstName);
+    public async getProviderByFirstName(firstName: string) {
+        return await ProviderRepository.findByFirstName(firstName);
     }
 
-    public saveProvider(providerParams: Provider) {
+    public async saveProvider(providerParams: Provider) {
         let provider = new Provider(providerParams);
-        return ProviderRepository.save(provider);
+        return await ProviderRepository.save(provider);
     }
 
-    public updateProvider(providerParams: Provider) {
+    public async updateProvider(providerParams: Provider) {
         let provider = new Provider(providerParams);
-        return ProviderRepository.update(provider);
+        return await ProviderRepository.update(provider);
     }
 
-    public deleteProviderById(id: string) {
+    public async deleteProviderById(id: string) {
         if (!id) {
             throw new Error('ID cannot be null or empty!');
         }
 
-        return ProviderRepository.deleteById(id);
+        return await ProviderRepository.deleteById(id);
     }
 }
 
